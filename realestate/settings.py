c389919e2d5f58e740c71d57d5ed41bd7f7576c2
torch <harall09@evergreen.edu>
2019-10-19 20:12:21 -0400

    models, migrations, and admin page setup

--- realestate/settings.py
+++ realestate/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realestatedb',
+        'USER': 'postgres',
+        'PASSWORD': 'P!n3apple$',
+        'HOST': 'localhost'
+
     }
 }
 
@@ -126,3 +130,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'realestate/static')
 ]
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
