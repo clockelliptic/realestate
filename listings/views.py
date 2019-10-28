9232af3c34a1438969cf787222575244bc53caf3
torch <harall09@evergreen.edu>
2019-10-28 16:16:21 -0400

    set up listing search functionality: search form choices, filterings results, and preserving form input

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import price_choices, state_choices, bedroom_choices
+
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
@@ -26,4 +28,44 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    if 'state' in request.GET:
+        search_params= request.GET['state']
+        if search_params:
+            queryset_list = queryset_list.filter(state__iexact=search_params)
+
+    if 'bedrooms' in request.GET:
+        # returns results UP TO the searched number of bedrooms
+        # because people are searching for what they can afford
+        search_params= request.GET['bedrooms']
+        if search_params:
+            queryset_list = queryset_list.filter(bedrooms__lte=search_params)
+
+    if 'price' in request.GET:
+        search_params= request.GET['price']
+        if search_params:
+            queryset_list = queryset_list.filter(price__lte=search_params)
+
+
+    context = \
+        {
+            'price_choices': price_choices,
+            'state_choices': state_choices,
+            'bedroom_choices': bedroom_choices,
+            'listings': queryset_list,
+            'values': request.GET
+        }
+
+    return render(request, 'listings/search.html', context)
